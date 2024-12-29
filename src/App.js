9b729e7ed013562d785564900408069435aac4f0
Celal <binaycelalselim@gmail.com>
2024-12-29 15:31:30 +0300

    Other files added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Gallery from "./components/Gallery/Gallery";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
